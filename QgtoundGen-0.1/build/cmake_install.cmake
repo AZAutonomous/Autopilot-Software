e48627c3dfffaae83d41a8a4cf04c8a53dd10654
james murphy <jamesmurphy@email.arizona.edu>
2017-09-20 17:13:04 -0700

    asdf

--- QgtoundGen-0.1/build/cmake_install.cmake
+++ QgtoundGen-0.1/build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/james/Desktop/autopilot
+# Install script for directory: C:/Users/james/Desktop/AZA/Autopilot-Software/QgtoundGen-0.1/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/autopilot_project")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Project")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,12 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/Users/james/Desktop/autopilot/build/src/cmake_install.cmake")
-
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -41,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/james/Desktop/autopilot/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/james/Desktop/AZA/Autopilot-Software/QgtoundGen-0.1/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
